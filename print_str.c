3b2f562662549d3d7f4d03206465c371205d6b5b
harishammache <hammache_haris@hotmail.fr>
2023-11-22 13:15:47 +0000

    send the projet 0

---
+++ print_str.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_str - Prints a string.
+ * @arg: A list of arguments pointing to the string
+ *
+ * Return: int if success
+ */
+int print_str(va_list arg)
+{
+	int index;
+	char *str = va_arg(arg, char *);
+
+	if (str == NULL)
+	{
+		return (-1);
+	}
+	for (index = 0; str[index]; index++)
+	{
+		_putchar(str[index]);
+	}
+	return (index);
+}

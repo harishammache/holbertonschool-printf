9eab8fa63a9fb514d89bffa52a6501cc161213e3
harishammache <hammache_haris@hotmail.fr>
2023-11-23 12:23:17 +0000

    Merge branch 'feature/MYFEATURE' into develop

---
+++ print_integer.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * print_int - Prints an integer
+ * @arg: A list of arguments pointing to the integer
+ *
+ * Return: Number of characters printed
+ */
+int print_integ(va_list arg)
+{
+    int num = va_arg(arg, int);
+    int number_total_characters = 0;
+
+    return number_total_characters;
+}
